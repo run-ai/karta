d58d1b93d7b4eb5e969f0fa45edd0e09e706ca51
Rotem Elad <relad@nvidia.com>
2025-08-13 19:15:35 +0300

    component spec improvements

--- pkg/api/v1alpha1/structure/component.go
+++ pkg/api/v1alpha1/structure/component.go
@@ -3,33 +3,34 @@ package structure
 import "k8s.io/apimachinery/pkg/runtime/schema"
 
 type ComponentDefinition struct {
-	Name                string                   `json:"name"`
-	Kind                *schema.GroupVersionKind `json:"kind,omitempty"`
-	OwnerName           *string                  `json:"ownerName,omitempty"`    // name of the owning component. nil if root?
-	SpecPath            *string                  `json:"specPath,omitempty"`     // JQ path to the component spec (e.g., '.spec.services[]' or '.spec.template') - defaults to '.spec', aka redundant for root components
-	MetadataPath        *string                  `json:"metadataPath,omitempty"` // JQ path to the component metadata. optional - defaults to '.metadata'
-	ChildSpecDefinition *ChildSpecDefinition     `json:"childSpecDefinition,omitempty"`
-	ScaleDefinition     *ScaleDefinition         `json:"scaleDefinition,omitempty"` // path to the scale/size struct
-	StatusDefinition    *StatusDefinition        `json:"statusDefinition,omitempty"`
-	References          []ReferenceDefinition    `json:"references,omitempty"` // list of components this component references
+	Name             string                   `json:"name"`
+	Kind             *schema.GroupVersionKind `json:"kind,omitempty"`
+	OwnerName        *string                  `json:"ownerName,omitempty"`    // name of the owning component. nil if root?
+	MetadataPath     *string                  `json:"metadataPath,omitempty"` // JQ path to the component metadata. optional - defaults to '.metadata'
+	SpecDefinition   *SpecDefinition          `json:"specDefinition,omitempty"`
+	ScaleDefinition  *ScaleDefinition         `json:"scaleDefinition,omitempty"` // path to the scale/size struct
+	StatusDefinition *StatusDefinition        `json:"statusDefinition,omitempty"`
+	References       []ReferenceDefinition    `json:"references,omitempty"` // list of components this component references
 }
 
-type ChildSpecDefinition struct {
+type SpecDefinition struct {
 	// only one of the following should be provided
-	PodTemplateSpecPath     *string                  `json:"podTemplateSpecPath,omitempty"`
-	PodSpecPath             *string                  `json:"podSpecPath,omitempty"`
+	PodTemplateSpecPath     *string                  `json:"podTemplateSpecPath,omitempty"`     // JQ path to the component pod template spec
+	PodSpecPath             *string                  `json:"podSpecPath,omitempty"`             // JQ path to the component pod spec
 	FragmentedPodDefinition *FragmentedPodDefinition `json:"fragmentedPodDefinition,omitempty"` // for cases where the parent component is not holding a podSpec or podTemplateSpec objects
 }
 
 type FragmentedPodDefinition struct {
-	SchedulerNamePath  *string `json:"schedulerNamePath"`
-	LabelsPath         *string `json:"labelsPath"`
-	AnnotationsPath    *string `json:"annotationsPath"`
-	ResourcesPath      *string `json:"resourcesPath"`
-	ResourceClaimsPath *string `json:"resourceClaimsPath"`
-	PodAffinityPath    *string `json:"podAffinityPath"`
-	NodeAffinityPath   *string `json:"nodeAffinityPath"`
-	ContainersPath     *string `json:"containersPath"`
+	SchedulerNamePath     *string `json:"schedulerNamePath,omitempty"`
+	LabelsPath            *string `json:"labelsPath,omitempty"`
+	AnnotationsPath       *string `json:"annotationsPath,omitempty"`
+	ResourcesPath         *string `json:"resourcesPath,omitempty"`
+	ResourceClaimsPath    *string `json:"resourceClaimsPath,omitempty"`
+	PodAffinityPath       *string `json:"podAffinityPath,omitempty"`
+	NodeAffinityPath      *string `json:"nodeAffinityPath,omitempty"`
+	ContainersPath        *string `json:"containersPath,omitempty"`
+	PriorityClassNamePath *string `json:"priorityClassNamePath,omitempty"`
+	ImagePath             *string `json:"imagePath,omitempty"`
 }
 
 type ReferenceDefinition struct {
