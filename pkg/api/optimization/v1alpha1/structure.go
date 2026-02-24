6834153c3a907d0f7ff331348e424a7d2fdc0fb0
Yuval Grauer <151432158+yuval-runai@users.noreply.github.com>
2026-02-24 13:57:46 +0200

    Files for OSS license (#19)

--- pkg/api/optimization/v1alpha1/structure.go
+++ pkg/api/optimization/v1alpha1/structure.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2026 NVIDIA Corporation
+
 package v1alpha1
 
 // GroupVersionKind represents a Kubernetes API object's group, version, and kind.
