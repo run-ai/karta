dfbe654ae0f8ccc8574afb19b56856f59b17bb12
sbouktus <sbouktus@nvidia.com>
2026-06-17 16:24:50 +0300

    yuval comment: change label values

--- pkg/api/runai/v1alpha1/labels.go
+++ pkg/api/runai/v1alpha1/labels.go
@@ -18,13 +18,13 @@ package v1alpha1
 const (
 	// LabelRootGroup is the API group of the root component
 	// (e.g. "ray.io", "kubeflow.org").
-	LabelRootGroup = "run.ai/karta-group"
+	LabelRootGroup = "karta.run.ai/group"
 
 	// LabelRootVersion is the API version of the root component
 	// (e.g. "v1", "v1alpha1").
-	LabelRootVersion = "run.ai/karta-version"
+	LabelRootVersion = "karta.run.ai/version"
 
 	// LabelRootKind is the kind of the root component
 	// (e.g. "RayCluster", "PyTorchJob").
-	LabelRootKind = "run.ai/karta-kind"
+	LabelRootKind = "karta.run.ai/kind"
 )
