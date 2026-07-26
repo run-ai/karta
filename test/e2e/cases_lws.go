a04c867d2f6ecd54ce3273bb4b93cce4e2633c12
aviadh <aviad.hayumi@gmail.com>
2026-07-26 21:09:17 +0300

    test(e2e): add lws scaled 1->2->1 flow
    Signed-off-by: aviadh <aviad.hayumi@gmail.com>

--- test/e2e/cases_lws.go
+++ test/e2e/cases_lws.go
@@ -8,6 +8,13 @@ var lwsCase = workloadCase{
 	operator:  "lws",
 	kartaFile: "../../docs/samples/lws.yaml",
 	kartaName: "leaderworkerset-x-k8s-io-leaderworkerset-v1",
-	states:    []namedState{{running, condTrue("Available")}},
-	flows:     []flow{{name: "running", workloadFile: "testdata/lws/running.yaml", journey: steps(running)}},
+	states: []namedState{{running, condTrue("Available")}},
+	flows: []flow{
+		{name: "running", workloadFile: "testdata/lws/running.yaml", journey: steps(running)},
+		{name: "scaled", workloadFile: "testdata/lws/scaled.yaml", journey: []step{
+			{state: running, settle: replicasReady(1), action: scaleReplicas(2)},
+			{state: running, settle: replicasReady(2), action: scaleReplicas(1)},
+			{state: running, settle: replicasReady(1)},
+		}},
+	},
 }
