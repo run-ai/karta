03cd82cbbc6b61f225c192c7b0c63f6b34060419
lavianalon <130892560+lavianalon@users.noreply.github.com>
2026-09-01 11:13:23 +0300

    fix(cli): render GPUs as allocated/requested, matching PODS
    Signed-off-by: lavianalon <130892560+lavianalon@users.noreply.github.com>

--- cli/pkg/generator/render.go
+++ cli/pkg/generator/render.go
@@ -72,7 +72,7 @@ func marshal(out io.Writer, views []workload.View, format Output) error {
 func renderTable(out io.Writer, views []workload.View, opts Options) error {
 	writer := printers.GetNewTabWriter(out)
 
-	headers := []string{"NAME", "NAMESPACE", "PHASE", "COMPONENTS", "PODS", "GPU-REQ", "GPU-ALLOC", "AGE"}
+	headers := []string{"NAME", "NAMESPACE", "PHASE", "COMPONENTS", "PODS", "GPUS", "AGE"}
 	if opts.Output == OutputWide {
 		headers = append(headers, "ORIGIN")
 	}
@@ -84,8 +84,7 @@ func renderTable(out io.Writer, views []workload.View, opts Options) error {
 			strings.Join(view.Phases, ","),
 			components(view.Components),
 			fmt.Sprintf("%d/%d", view.PodStats.PodsRunning, view.PodStats.PodsTotal),
-			fmt.Sprint(view.GPUs),
-			fmt.Sprint(view.PodStats.AllocatedGPUs),
+			fmt.Sprintf("%d/%d", view.PodStats.AllocatedGPUs, view.GPUs),
 			age(now, view.CreatedAt),
 		)
 		if opts.Output == OutputWide {
